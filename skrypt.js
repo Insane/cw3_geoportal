57a19f910c3972640f2fa6ad9f2d75336ee4282a
Paulina B. <paulina.bednar@gmail.com>
2014-11-14 18:02:20 +0100

    Geoportal - prace wstepne
    Dodanie map podkladowych

--- skrypt.js
+++ skrypt.js
@@ -1,11 +1,45 @@
 $(function(){
-	var map = L.map('mapa');
+	var warstwa_osm=L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+   	 attribution: 'Źródło danych; <a href="http://openstreetmap.org">OpenStreetMap</a>',
+	}),
+		warstwa_esriTopo=L.esri.basemapLayer('Topographic'),
+		warstwa_esriSat=L.esri.basemapLayer('Imagery');
+		etykiety=  L.esri.basemapLayer('ImageryLabels');
+		
+	var map = L.map('mapa',{
+		center: [52,18],
+		zoom:6,
+		zoomControl:false,
+		layers:[warstwa_osm]
+		});
+	skala=L.control.scale({
+		position:"bottomleft",
+		metric: true,
+		maxWidth: 100,
+		updateWhenIdle:true,
+		imperial:false
+		});
+	skala.addTo(map);
+	
+	var baseMaps={
+		"OpenStreetMap":warstwa_osm,
+		"Mapa topograficzna (ESRI)":warstwa_esriTopo,
+		"Zdjęcia satelitarne (ESRI)":warstwa_esriSat
+		};
+	var inne={
+		"Etykiety (ESRI)":etykiety
+	};
+	L.control.layers(baseMaps,inne,{
+		collapsed:false,
+		position:"topleft"
+	}).addTo(map);
+	
+	L.control.zoom({
+		position:"bottomleft",
+		zoomInTitle:"Przybliż",
+		zoomOutTitle:"Oddal"
+	}).addTo(map);
+	
 
-	warstwa=L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-   	 attribution: 'Źródło danych; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 10
-});
-	map.setView(new L.LatLng(52,21),6);
-	map.addLayer(warstwa);
 	
 });	
