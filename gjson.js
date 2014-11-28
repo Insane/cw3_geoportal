23ad2c85898bd6c29f787bf3466b8fa1834ffbc0
Paulina B <paulina.bednar@gmail.com>
2014-11-28 10:10:11 +0100

    Geoportal - dalsze prace
    Dodanie:
    - tematyki geoportalu (Gorzów takie piekne miasto)
    - ciekawych miejsc gorzowskich (obiekty geojson -gjson.js)
    - drobne korekty

---
+++ gjson.js
@@ -0,0 +1,162 @@
+var miejscaGW = [{
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+      	"nazwa":"Bulwar Nadwarciański zachodni",
+      	"opis":"Cudowne miejsce na relaks i odpoczynek. Można napic się piwa, spędzić czas na mini plaży i podziwiać widoki.",
+      	"linki":"http://pl.wikipedia.org/wiki/Bulwar_Nadwarcia%C5%84ski_w_Gorzowie_Wielkopolskim",
+      	"zdjecie":"http://radiogorzow.fm/wp-content/blogs.dir/7/files/gallery/czas-na-bulwar/bulwar-zachodni-i-widok_-na_-warte-fot_-daniel-adamski.jpg"
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.235322713851929,52.72832015959868], [15.235569477081299,52.728086255056354],[15.240021944046019,52.72935322298494],[15.239657163619995,52.72939220602971],[15.235322713851929,52.72832015959868]]]
+}
+    },
+    {
+      "type": "Feature",
+      "properties": {"nazwa":"Bulwar Nadwarciański wchodni",
+      	"opis":"Cudowne miejsce na relaks i odpoczynek. Umieszczono tu ciekawą mapę miasta oraz klatkę z egzotycznymi ptakami.",
+      	"linki":"http://pl.wikipedia.org/wiki/Bulwar_Nadwarcia%C5%84ski_w_Gorzowie_Wielkopolskim",
+      	"zdjecie":"http://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Gorzow_Bulwar_Nadwarcianski_E.jpg/240px-Gorzow_Bulwar_Nadwarcianski_E.jpg"
+    },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [
+  [[15.240504741668701,52.72963909783717],[15.240579843521118,52.72956762929984],[15.24349808692932,52.73038626189499],[15.245203971862791,52.73074359670085],[15.245150327682497,52.73083455418381],[15.242704153060913,52.730379764871394],[15.240504741668701,52.72963909783717]]]
+}},
+    {
+      "type": "Feature",
+      "properties": {"nazwa":"Katedra Wniebowzięcia Najświętszej Maryi Panny",
+      		"opis":"Najstarsza świątynia wzniesiona pod koniec XIII wieku na planie trzynawowego, pseudobazylikowego korpusu.",
+      		"linki":"http://pl.wikipedia.org/wiki/Katedra_Wniebowzi%C4%99cia_Naj%C5%9Bwi%C4%99tszej_Maryi_Panny_w_Gorzowie_Wielkopolskim",
+       		"zdjecie":"http://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Dom_Gorzow_Wielkopolski.jpg/800px-Dom_Gorzow_Wielkopolski.jpg"
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.238978564739227,52.73090764488082],[15.239026844501497,52.730756590638585],[15.239694714546202,52.73081831179006],[15.239700078964232,52.730847548094445],[15.239855647087097,52.73087191166647],[15.239850282669067,52.73093363265469],[15.239654481410978,52.730920638769675],[15.239641070365908,52.73099210508922],[15.239343345165251,52.73096449311602],[15.239329934120178,52.73091739029784],[15.238978564739227,52.73090764488082]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"nazwa":"Gsleria Askana",
+      	"opis":"3-poziomowa galeria hanflowa, miejsce spotkań w Gorzowie. Można tu zrobić zakupy oraz pójść do kina Helios.",
+      	"linki":"http://www.galeria-askana.pl/index",
+      	"zdjecie":"http://upload.wikimedia.org/wikipedia/commons/thumb/7/7b/Askana_Gorz%C3%B3w_Wlkp..jpg/240px-Askana_Gorz%C3%B3w_Wlkp..jpg"
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.224668979644774,52.731477748112276],[15.224100351333616,52.731204879119694],[15.224078893661497,52.73104245628945],[15.224518775939943,52.73089302675121],[15.226428508758545,52.73069811788819],[15.227801799774172,52.73111392240931],[15.227555036544802,52.73134131382947],[15.226964950561522,52.7311464069705],[15.226256847381592,52.73126335119044],[15.226310491561888,52.73158169775547],[15.225666761398317,52.731835073972455],[15.225183963775635,52.7317636090347],[15.224668979644774,52.731477748112276]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"nazwa":"Mur obronny",
+      	"opis":"Mur obronny z XV wieku",
+      	"linki":"http://wrotalubuskie.eu/PL/obiekt_turystyczny/szczegoly/524/197/Mury_obronne_i_fortyfikacje_miejskie_w_Gorzowie_Wielkopolskim/",
+      	"zdjecie":"http://giik.pl/UserFiles/Image/mur3.jpg"
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.238916873931885,52.73268289906785],[15.239013433456421,52.73263092559001],[15.239179730415344,52.732747865827974],[15.239871740341185,52.732809584160435],[15.240397453308105,52.73276410750289],[15.240890979766846,52.732669905704185],[15.240944623947142,52.732734872483704],[15.240676403045654,52.73284531578667],[15.240059494972229,52.73289404068433],[15.239512324333191,52.73286805407901],[15.239050984382628,52.73278359750479],[15.238916873931885,52.73268289906785]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {      	
+      	"nazwa":"Teatr im. Juliusza Osterwy",
+      	"opis":"Teatr istniejący od 1946 roku",
+      	"linki":"http://www.teatr-gorzow.pl/",
+      	"zdjecie":"http://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Gorz%C3%B3w_Wielkopolski%2C_Teatr_Juliusza_Osterwy.jpg/240px-Gorz%C3%B3w_Wielkopolski%2C_Teatr_Juliusza_Osterwy.jpg"
+},
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.246899127960205,52.73239704417306],[15.246872305870054,52.732293096473896],[15.247387290000916,52.73226386113884],[15.247457027435303,52.732364560543694],[15.247204899787905,52.73240678925714],[15.247043967247011,52.73241003761804],[15.246899127960205,52.73239704417306]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	 "nazwa":"Filharmonia Gorzowska",
+      	"opis":"Gmach powstał w miejscu kultowego kina Kopernikm, obecnie nic ciekawego się tam nie dzieje, ale ładnie wygląda",
+      	"linki":"http://www.filharmoniagorzowska.pl/",
+      	"zdjecie":"http://upload.wikimedia.org/wikipedia/commons/thumb/5/5b/Filharmonia_Gorzowska.jpg/240px-Filharmonia_Gorzowska.jpg"
+
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.246598720550535,52.73685032063453],[15.246888399124146,52.73602531323154],[15.247156620025635,52.735953855162634],[15.247457027435303,52.736057794133245],[15.247253179550171,52.73661646185408],[15.247242450714111,52.737006225791134],[15.246984958648682,52.73709067418479],[15.246598720550535,52.73685032063453]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"nazwa":"NovaPark",
+      	"opis":"Wielka galeria handlowa, w której kwitnie życie gorzowskie. Tam też powstało pierwsze w Gorzowie KFC.",
+      	"linki":"http://nova-park.pl/",
+      	"zdjecie":"http://gorzowianie.com.pl/userzy_foto/32/tematy/352/duze.jpg"
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.242264270782469,52.725974560030785],[15.244753360748291,52.7268842258106],[15.24348735809326,52.72801478397369],[15.24250030517578,52.72794981016052],[15.24250030517578,52.72770290878752],[15.24179220199585,52.72744301109504],[15.242114067077637,52.72713113181926],[15.241534709930422,52.72691021598261],[15.242264270782469,52.725974560030785]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"nazwa":"Aquapark Słowianka",
+      	"opis":"Kompleks basenowy, czynny cały rok. Nieopodal znajdują się również korty tenisowe, a zimą lodowisko.",
+      	"linki":"http://www.slowianka.pl/",
+      	"zdjecie":"http://i.nocimg.pl/d-3/83/23-gorzow-wielkopolski-aguapark-slowianka-gorzow-wielkopolski.jpg"
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.217738151550293,52.74478778291872],[15.21730899810791,52.7442032400825],[15.218145847320557,52.74391746073105],[15.218532085418701,52.744099320535156],[15.218832492828367,52.7444630378666],[15.217738151550293,52.74478778291872]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"nazwa":"Punkt widokowy - Schody do nikąd",
+      	"opis":"Urocze miejsce na spacery, znajdujące się w obszernym parku im. Siemiradzkiego. W parku znajduje się również Amfiteatr.",
+      	"linki":"http://pl.wikipedia.org/wiki/Park_im._Siemiradzkiego_%28Gorz%C3%B3w_Wielkopolski%29",
+      	"zdjecie":"http://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Schody_do_nik%C4%85d_Gorz%C3%B3w_Wielkopolski2.jpg/240px-Schody_do_nik%C4%85d_Gorz%C3%B3w_Wielkopolski2.jpg"
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.240772962570189,52.733962726410184],[15.240740776062012,52.73376458301081],[15.241320133209229,52.733637900693346],[15.241641998291016,52.73383604466853],[15.241186022758482,52.734144627754425],[15.240810513496399,52.734056925415345],[15.240772962570189,52.733962726410184]]]
+        }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	      	"nazwa":"1LO im. Tadeusza Kościuszki ",
+      	      	"opis":"Najlepsze liceum w Gorzowie!",
+      	"linki":"http://www.zso1.edu.gorzow.pl/",
+      	"zdjecie":"http://www.zso1.edu.gorzow.pl/readfoto.php?img=20081111153225_4919977987d6b.jpg"
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.234990119934082,52.75204195646977],[15.234925746917723,52.75202896887663],[15.234732627868652,52.75186012981355],[15.234410762786865,52.75187311745702],[15.234249830245972,52.75091851534608],[15.234721899032593,52.75089903345256],[15.23505449295044,52.75189909273233],[15.235075950622559,52.75204845026487],[15.234990119934082,52.75204195646977]]]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+      	"nazwa":"2LO im. Marii Skłodowskiej-Curie",
+      	"opis":"Najgorsze liceum w Gorzowie!",
+      	"linki":"http://2lo.gorzow.pl/",
+      	"zdjecie":"http://bi.gazeta.pl/im/2d/89/d3/z13863213Q,II-LO-w-Gorzowie.jpg"
+      },
+      "geometry": {
+"type": "Polygon",
+"coordinates": [[[15.245633125305174,52.72271910059646],[15.246416330337524,52.722953033929244],[15.245944261550902,52.72332342580657],[15.24551510810852,52.723154475515884],[15.245289802551268,52.72306350201132],[15.245633125305174,52.72271910059646]]]
+      }
+    }
+  ]
+}
+];
+
--- skrypt.js
+++ skrypt.js
@@ -8,8 +8,8 @@ $(function(){
 		etykiety=  L.esri.basemapLayer('ImageryLabels');
 		
 	var map = L.map('mapa',{
-		center: [52,18],
-		zoom:6,
+		center: [52.731767,15.238472],
+		zoom:14,
 		zoomControl:false,
 		layers:[warstwa_osm]
 		});
@@ -41,10 +41,31 @@ $(function(){
 		zoomOutTitle:"Oddal"
 	}).addTo(map);
 	
+function onEachFeature(feature,layer){
+	if(feature.properties){
+		layer.bindPopup("<table><tr><td><b>"+feature.properties.nazwa+"</b><br>"+feature.properties.opis+"<br></td><td><img src="+feature.properties.zdjecie+" width=100 height=100/></td></tr><tr><td></td><td></tr></table><a target=&quot;_blank&quot; href="+feature.properties.linki+"> Dowiedz się więcej o tym miejscu! </a>");
+	}
+}
+var miejsca = new L.geoJson(miejscaGW, {
+	onEachFeature:onEachFeature
+   });
+var licznik_miejsca=0;
+    $("#miejsca").click(function(){
+    	licznik_miejsca+=1;
+    	if(licznik_miejsca%2==0){
+    		$("#miejsca").css("opacity",1);
+    		map.removeLayer(miejsca);
+    	}else
+			miejsca.addTo(map);
+			$("#miejsca").css("opacity",0.5);
+	});
+
+    var punkty = new L.FeatureGroup();
+    
 	function markerki(){
 		var markery=[];
 		map.on("click",function(e){
-			var marker = new L.marker(e.latlng).addTo(map);
+			var marker = new L.marker(e.latlng).addTo(punkty);
 			marker.bindPopup("Lat: "+Math.round(e.latlng.lat*1000000)/1000000 + " Lng: "+Math.round(e.latlng.lng*1000000)/1000000);
 			marker.addTo(markery);
 			if (markery.index(marker)==-1){
@@ -56,17 +77,24 @@ $(function(){
 				};
 			}}
 		});
+		map.addLayer(punkty);
 	}
+	var licznik_markery=0;
 	$("#punkt").click(function(){
+		licznik_markery+=1;
+		if (licznik_markery%2==0){
+				$("#czysc").hide();
+				$("#punkt").css("opacity",1);
+		}else{		
 		markerki();
 		$("#czysc").show();
 		$("#punkt").css("opacity",0.5);
+		}
 	});
-	
 	
 	$("#czysc").click(function(){
-		for (i=0;i<markery.length;i++){
-				map.remove(markery);
-		};
+		map.removeLayer(punkty);
 	});
+	
+	
 });	
